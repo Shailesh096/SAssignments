dc5160080bb3589867b757024ab91cf93e509145
shailesh <shaileshsab123@gamil.com>
2025-12-29 13:13:13 +0530

    Assignment02

---
+++ Assignment02/Q1/SortByRollNo.java
@@ -0,0 +1,26 @@
+package Assignment2que1;
+
+
+
+import java.util.Comparator;
+
+class SortByRollNo implements Comparator<Student> {
+ @Override
+ public int compare(Student a, Student b) {
+     return a.getRollNo() - b.getRollNo();
+ }
+}
+
+class SortByName implements Comparator<Student> {
+ @Override
+ public int compare(Student a, Student b) {
+     return a.getName().compareToIgnoreCase(b.getName());
+ }
+}
+
+class SortByMarks implements Comparator<Student> {
+ @Override
+ public int compare(Student a, Student b) {
+     return Double.compare(b.getMarks(), a.getMarks());
+ }
+}
---
+++ Assignment02/Q1/Student.java
@@ -0,0 +1,32 @@
+package Assignment2que1;
+
+
+
+public class Student {
+    private int rollNo;
+    private String name;
+    private double marks;
+
+    public Student(int rollNo, String name, double marks) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Roll No: " + rollNo + " | Name: " + name + " | Marks: " + marks;
+    }
+}
